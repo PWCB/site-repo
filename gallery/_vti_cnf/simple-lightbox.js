8f2abdcce1e27e4908884d28809c7f8109386bf5
PWCB <pwcbrock@gmail.com>
2018-01-15 10:03:28 -0700

    commit1
    preloading backgrounds

--- gallery/_vti_cnf/simple-lightbox.js
+++ gallery/_vti_cnf/simple-lightbox.js
@@ -1,4 +1,4 @@
 vti_encoding:SR|utf8-nl
 vti_timelastmodified:TR|14 Jan 2018 06:17:02 -0000
 vti_extenderversion:SR|12.0.0.0
-vti_backlinkinfo:VX|artwork.html music.html master.dwt about.html misc.html games.html photos.html index.html
+vti_backlinkinfo:VX|artwork.html site-repo/photos.html music.html master.dwt about.html misc.html site-repo/artwork.html site-repo/index.html games.html photos.html site-repo/about.html site-repo/misc.html site-repo/music.html index.html site-repo/games.html site-repo/master.dwt
