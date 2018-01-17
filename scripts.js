df5f5c0e99d4ce1b3425ca4d400e8352cddb76a0
PWCB <pwcbrock@gmail.com>
2018-01-17 09:32:26 -0700

    commit

--- scripts.js
+++ scripts.js
@@ -1,8 +1,8 @@
 function randombg(){
   var random= Math.floor(Math.random() * 4) + 0;
-  var bigSize = ["url('bg0.png')",
-                 "url('bg1.png')",
-                 "url('bg2.png')",
-                 "url('bg3.png')"];
+  var bigSize = ["url('https://i.imgur.com/cQBz64F.png')",
+                 "url('https://i.imgur.com/YEtCxH4.jpg')",
+                 "url('https://i.imgur.com/x8B9VDQ.png')",
+                 "url('https://i.imgur.com/NJa7ixC.png')"];
   document.getElementById("random").style.backgroundImage=bigSize[random];
 }
